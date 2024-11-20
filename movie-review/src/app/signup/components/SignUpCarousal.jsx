febee8d7d5afa83c98d4bdbf18a9091bef676659
Eshan Goel <67696491+Eshan27@users.noreply.github.com>
2024-11-20 16:19:50 -0500

    signup ui additions

--- movie-review/src/app/signup/components/SignUpCarousal.jsx
+++ movie-review/src/app/signup/components/SignUpCarousal.jsx
@@ -3,7 +3,28 @@ import './SignUp.css'
 
 function SignUpCarousal() {
   return (
-    <div className="carousal-container">SignUp Carousal</div>
+    <div className="carousal-container">
+      <div className='subtitle'> 
+        Welcome to Worth-a-Share, your go-to destination
+        for in-depth movie reviews, ratings, and discussions!
+        <br />
+        <br />
+        Here, you&apos;ll find a community of fellow movie lovers, all<br />
+        eager to explore, discuss, and share their perspectives<br />
+        on the latest releases and timeless classics.
+        <br />
+        <br />
+        By signing up, you&apos;ll unlock personalized <br />
+        recommendations, access to exclusive content, and the<br />
+        opportunity to contribute your own reviews and ratings.
+        <br/>
+        <br />
+        <b>
+          Get ready to dive into the world of cinema-your next<br />
+          favourite movie awaits!
+        </b>
+      </div>
+    </div>
   )
 }
 
