f7870002aaaa5c93f9fba36f5f3c176bd9c50bb7
Eshan Goel <67696491+Eshan27@users.noreply.github.com>
2024-11-25 14:36:10 -0500

    signup-carousal added

--- movie-review/src/app/signup/components/SignUpCarousal.jsx
+++ movie-review/src/app/signup/components/SignUpCarousal.jsx
@@ -1,9 +1,48 @@
 import React from 'react';
 import './SignUp.css'
+import Image from 'next/image';
 
 function SignUpCarousal() {
+  const moviePosters = [
+    '/posters/12thFail.jpg',
+    '/posters/darkKnight.jpg',
+    '/posters/GOW.jpeg',
+    '/posters/interstellar.jpg',
+    '/posters/maharaja.jpg',
+    '/posters/myNameIsKhan.jpg',
+    '/posters/oppenheimer.jpg',
+    '/posters/parasite.jpg',
+    '/posters/seven.jpg',
+    '/posters/shawshank.jpeg',
+  ];
   return (
     <div className="carousal-container">
+      <div className="carousel">
+        <div className="carousel-track">
+          {moviePosters.map((poster, index) => (
+            <Image
+              key={index}
+              src={poster}
+              alt={`Movie Poster ${index + 1}`}
+              className="carousel-poster"
+              width={1000}
+              height={1000}
+              // fill={true}
+            />
+          ))}
+          {/* Duplicate posters for infinite scrolling */}
+          {moviePosters.map((poster, index) => (
+            <Image
+              key={`duplicate-${index}`}
+              src={poster}
+              alt={`Duplicate Poster ${index + 1}`}
+              className="carousel-poster"
+              width={500}
+              height={500}
+            />
+          ))}
+        </div>
+      </div>
       <div className='subtitle'> 
         Welcome to Worth-a-Share, your go-to destination
         for in-depth movie reviews, ratings, and discussions!
